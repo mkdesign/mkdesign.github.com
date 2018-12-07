5abf812178193768fcd167a30875608916aca897
mkdesign <mehrad.kavian@gmail.com>
2018-12-07 11:59:55 -0500

    stupid animation

--- scripts.js
+++ scripts.js
@@ -34,5 +34,32 @@ $(function(){
 			$('article.about, section.social-hubs').removeClass('mapOpen');
 		})
 	})
+
+	// var animateWord = $('article.about p.about').text();
+
+	// function animatedIt () {
+	// 	var newWord= '';
+	// 	for(var i =0 ; i< animateWord.length; i++) {
+	// 		var subStr = animateWord.substr(i,1);
+	// 		if(subStr != ' ') 
+	// 			 newWord += '<span>'+subStr+'</span>';
+	// 		else 
+	// 			newWord+=subStr
+	// 	}
+
+	// 	$('article.about p.about').html(newWord);
+
+	// 	var animateDelay = 100;
+
+	// 	$('article.about p.about > span').each(function(){
+	// 		var that = this;
+	// 		setTimeout(function(){
+	// 			$(that).animateCss('fadeInDown').css({'opacity' : 1});
+				
+	// 		},animateDelay)
+	// 		animateDelay+=50;
+	// 	})
+	// }
+	// animatedIt();
 	
 })
